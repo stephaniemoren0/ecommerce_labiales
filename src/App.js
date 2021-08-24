eaeaba59fe5027f78fe8fd277fbfb32e5ecf046b
ilse moreno <ilsemoreno@Stephanie-Moreno.local>
2021-08-23 21:03:19 -0500

    se creo menu, htmls de las secciones

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+          <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>si</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +19,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+function NavBar(){
+    return(
+        <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" >
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">Home</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="html/labiales.html">Labiales</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="html/paletas.html">Paletas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="html/bases.html">Bases</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="html/delineadores.html">Delineadores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="html/otros.html">Otros</a>
+                        </li>
+                    </ul>
+                    <div class="col-4">
+                        <form class="d-flex" id="miBusqueda">
+                            <input id="filtrado" onkeyup="buscarProducto()" class="form-control me-2" type="search" placeholder="Estoy buscando..." aria-label="Search" /></form>
+                    </div>
+                    <div>
+                        <div class="dropdown">
+                            <button class="btn btnColor dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                🛒 Carrito <span id="notificacionCarrito" class="badge badge-dark">0</span>
+                            </button>
+                            <div id="productosCarrito" class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                </nav>
+                </header>
+    )
+}
+
+export default NavBar
