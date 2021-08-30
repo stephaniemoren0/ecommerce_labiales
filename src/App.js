0929956f1b5cbba5804183afcb024a39c4ecb3f0
ilse moreno <ilsemoreno@Stephanie-Moreno.local>
2021-08-29 22:12:56 -0500

    se regresaron los cambios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { NavBar} from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
+import {NavBar, carrito} from './components/NavBar';
+import {ItemListContainer, cosmetico} from './components/ItemListContainer';
 
 function App() {
 
   return (
     <div className="App">
         <NavBar />
+        <ItemListContainer cosmeticonuevo={cosmetico} />
         <br />
-
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <ItemListContainer greeting={'¡Hola Mate!,muchas gracias por la ayuda, no se si esto es demasiado sencillo o yo muy tonta jajaja 😆'}/>
         </p>
         <a
           className="App-link"
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,30 @@
-const ItemListContainer =(props)=>{
-    const {greeting}= props
-    return(
-        <h1>{greeting}</h1>
-    )
+export const ItemListContainer=({cosmeticonuevo})=>{
+    return (
+    <div>
+        <br />
+        <br />
+        <br />
+        <div class="col-4">
+            <div class="card">
+    <img src={cosmeticonuevo.imagen} class="card-img-top trans imagenContenedor bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid" alt="cosmetico" />
+                <div class="card-body">
+            <h5 class="card-title">{cosmeticonuevo.nombre}</h5>
+            <p class="card-text">{cosmeticonuevo.descripcion}</p>
+            <p class="card-text">Color {cosmeticonuevo.color}</p>
+            <h2 class="card-title"> Precio $ {cosmeticonuevo.precio} </h2> 
+            <a href="#"  class="btn btn-color btn-primary btn-comprar">Comprar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+
 }
 
-export default ItemListContainer
-
-
-
-
+export const cosmetico ={
+    nombre:'Labial Graduable Mate Studio Look',
+    descripcion:'¡Tú decides la intensidad! Labial en barra con acabado mate de textura ultra ligera',
+    color:'wine',
+    precio:109.90,
+    imagen:'https://cyzone.tiendabelcorp.com/cdn-cgi/image/width=1200,fit=contain,f=auto/https://belc-bigdata-mdm-images-prd.s3.amazonaws.com/images/FotoProductoFondoBlancoWebRedes/200098795_fotofondoblanco.jpg'
+}
